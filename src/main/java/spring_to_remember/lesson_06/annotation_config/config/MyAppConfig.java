bd52ee0036683e1d963804762f7710d3f01f8117
Oleg Chumin <tschumin.oleg@gmail.com>
2024-06-20 20:45:48 +0300

    Add lesson_06

---
+++ src/main/java/spring_to_remember/lesson_06/annotation_config/config/MyAppConfig.java
@@ -0,0 +1,23 @@
+package spring_to_remember.lesson_06.annotation_config.config;
+
+import org.springframework.context.annotation.*;
+import spring_to_remember.lesson_06.annotation_config.models.Cat;
+import spring_to_remember.lesson_06.annotation_config.models.Pet;
+import spring_to_remember.lesson_06.annotation_config.service.Person;
+
+@Configuration
+//@ComponentScan("spring_to_remember.lesson_06.annotation_config")
+@PropertySource("classpath:myApp.properties")
+public class MyAppConfig {
+
+    @Bean
+    @Scope
+    public Pet catBean() {
+        return new Cat();
+    }
+
+    @Bean
+    public Person personBean() {
+        return new Person(catBean());
+    }
+}
